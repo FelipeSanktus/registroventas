b624dbcd04dd334ea9a6c8c4444fdd9f72f25c60
Felipe Sanchez Ortiz <felipe.sanchez.o@usach.cl>
2021-02-24 20:36:05 -0300

    Test nueva configuracion para cors

--- src/main/java/avla/registroventas/security/WebSecurity.java
+++ src/main/java/avla/registroventas/security/WebSecurity.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +13,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -33,17 +40,46 @@ public class WebSecurity  extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().
-                disable().
+        http.
+                cors().
+                and().
                 authorizeRequests().
-                antMatchers("/login").
-                permitAll().
                 anyRequest().
                 authenticated().
                 and().
                 addFilterBefore(new LoginFilter("/login",authenticationManager()), UsernamePasswordAuthenticationFilter.class).
                 addFilterAfter(new JwtFilter(),UsernamePasswordAuthenticationFilter.class);
 
+    }
+
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+
+        // NOTE: setAllowCredentials(true) is important,
+        // otherwise, the value of the 'Access-Control-Allow-Origin' header in the response
+        // must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+
+        // NOTE: setAllowedHeaders is important!
+        // Without it, OPTIONS preflight request will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(Arrays.asList(
+                "Authorization",
+                "Accept",
+                "Cache-Control",
+                "Content-Type",
+                "Origin",
+                "ajax", // <-- This is needed for jQuery's ajax request.
+                "x-csrf-token",
+                "x-requested-with"
+        ));
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
